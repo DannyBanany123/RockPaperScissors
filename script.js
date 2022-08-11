3795e7e98903946040961895ab6e35a47b6f3d92
DannyBanany123 <ngodanny2@gmail.com>
2022-08-11 04:01:42 -0500

    Create title header CSS

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-function getComputerChoice() {
-    let number = Math.floor((Math.random() * 3) + 1);
-    let choice;
-    if (number === 1) {
-        choice = "rock";
-    } else if (number === 2) {
-        choice = "paper";
-    } else {
-        choice = "scissors";
-    }
-    return choice;
-}
+// function getComputerChoice() {
+//     let number = Math.floor((Math.random() * 3) + 1);
+//     let choice;
+//     if (number === 1) {
+//         choice = "rock";
+//     } else if (number === 2) {
+//         choice = "paper";
+//     } else {
+//         choice = "scissors";
+//     }
+//     return choice;
+// }
 
-function playRound(play, computer) {
-    let player = play.toLowerCase();
-    let result = "Tie!";
-    if (player === "rock") {
-        if (computer === "paper") {
-            result = "You lose! Paper beats rock.";
-        } else if (computer === "scissors") {
-            result = "You win! Rock beats scissors.";
-        }
-    } else if (player === "paper") {
-        if (computer === "scissors") {
-            result = "You lose! Scissors beats paper."
-        } else if (computer === "rock") {
-            result = "You win! Paper beats rock.";
-        }
-    } else {
-        if (computer === "paper") {
-            result = "You win! Scissors beats paper.";
-        } else if (computer === "rock") {
-            result = "You lose! Rock beats scissors.";
-        }
-    }
-    return result;
-}
+// function playRound(play, computer) {
+//     let player = play.toLowerCase();
+//     let result = "Tie!";
+//     if (player === "rock") {
+//         if (computer === "paper") {
+//             result = "You lose! Paper beats rock.";
+//         } else if (computer === "scissors") {
+//             result = "You win! Rock beats scissors.";
+//         }
+//     } else if (player === "paper") {
+//         if (computer === "scissors") {
+//             result = "You lose! Scissors beats paper."
+//         } else if (computer === "rock") {
+//             result = "You win! Paper beats rock.";
+//         }
+//     } else {
+//         if (computer === "paper") {
+//             result = "You win! Scissors beats paper.";
+//         } else if (computer === "rock") {
+//             result = "You lose! Rock beats scissors.";
+//         }
+//     }
+//     return result;
+// }
 
-function game() {
-    playerCount = 0;
-    cpuCount = 0;
-    while (playerCount < 5 && cpuCount < 5) {
-        let play = prompt("Please choose Rock, Paper, or Scissors: ");
-        let cpu = getComputerChoice();
-        let result = playRound(play, cpu);
-        console.log(result);
-        if (result.length > 4) {
-            if (result.substring(0, 7) === "You win") {
-                playerCount++;
-            } else {
-                cpuCount++;
-            }
-        }
-    }
-    if (playerCount === 5) {
-        console.log("Victory! You were the first to 5 wins.");
-    } else {
-        console.log("Defeated! Computer was the first to 5 wins.");
-    }
-}
+// function game() {
+//     playerCount = 0;
+//     cpuCount = 0;
+//     while (playerCount < 5 && cpuCount < 5) {
+//         let play = prompt("Please choose Rock, Paper, or Scissors: ");
+//         let cpu = getComputerChoice();
+//         let result = playRound(play, cpu);
+//         console.log(result);
+//         if (result.length > 4) {
+//             if (result.substring(0, 7) === "You win") {
+//                 playerCount++;
+//             } else {
+//                 cpuCount++;
+//             }
+//         }
+//     }
+//     if (playerCount === 5) {
+//         console.log("Victory! You were the first to 5 wins.");
+//     } else {
+//         console.log("Defeated! Computer was the first to 5 wins.");
+//     }
+// }
 
-let playerCount = 0;
-let cpuCount = 0;
-game();
+// let playerCount = 0;
+// let cpuCount = 0;
+// game();
