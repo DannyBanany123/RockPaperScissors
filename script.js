41d9f79148e6100358f2104c7a2b1b498c9c6d69
DannyBanany123 <ngodanny2@gmail.com>
2022-08-11 13:57:15 -0500

    Remove while loop, add feature to play one round everytime user clicks rps button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
 }
 
 function playRound(player, computer) {
-    let player = play.toLowerCase();
+    let play = player.toLowerCase();
     let result = "Tie!";
     if (player === "rock") {
         if (computer === "paper") {
@@ -36,38 +36,32 @@ function playRound(player, computer) {
     return result;
 }
 
-function game() {
-    playerCount = 0;
-    cpuCount = 0;
-    while (playerCount < 5 && cpuCount < 5) {
-        let play = getPlayerChoice();
-        let cpu = getComputerChoice();
-        let result = playRound(play, cpu);
-        console.log(result);
-        if (result.length > 4) {
-            if (result.substring(0, 7) === "You win") {
-                playerCount++;
-            } else {
-                cpuCount++;
-            }
+function game(playerChoice) {
+    let play = playerChoice;
+    let cpu = getComputerChoice();
+    let result = playRound(play, cpu);
+    console.log(result);
+    if (result.length > 4) {
+        if (result.substring(0, 7) === "You win") {
+            playerCount++;
+        } else {
+            cpuCount++;
         }
     }
-    if (playerCount === 5) {
-        console.log("Victory! You were the first to 5 wins.");
-    } else {
-        console.log("Defeated! Computer was the first to 5 wins.");
-    }
-}
-
-function getPlayerChoice() {
-    const choices = document.querySelectorAll(".buttons div");
-    choices.forEach((choice) => {
-        choice.addEventListener("click", () => {
-            return choice.getAttribute("class");
-        });
-    });
 }
 
 let playerCount = 0;
 let cpuCount = 0;
-game();
+let pick = "";
+const choices = document.querySelectorAll(".buttons > div");
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        pick = choice.getAttribute("class");
+        game(pick);
+    });
+});
+if (playerCount === 5) {
+    console.log("Victory! You were the first to 5 wins.");
+} else if (cpuCount === 5) {
+    console.log("Defeated! Computer was the first to 5 wins.");
+}
